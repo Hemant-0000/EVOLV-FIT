f667ba7b226af23c937d42212fe66fb1da981e90
Hemant <hrsmart2150@gmail.com>
2022-07-28 00:38:11 +0530

    Made Row compo and mapped it in MainConatiner

---
+++ components/Row.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from 'react'
+import date_icon from '../public/assets/date.svg'
+import bell from '../public/assets/bell.svg'
+import { FiPlus, FiMinus } from 'react-icons/fi'
+import { TbCalendarTime } from 'react-icons/tb'
+import { AiOutlineExclamation } from 'react-icons/ai'
+import { IoIosArrowForward } from 'react-icons/io'
+import { Progress } from '@mantine/core';
+import { PieChart } from 'react-minimal-pie-chart';
+import { buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import Image from 'next/image'
+
+const Row = ({ id, userImage, username, email, stepsWalked, stepsTarget, performedDate, scheduledDate, calories, caloriesTarget, protein, fats, carbs, dataMock, feedback }) => {
+
+    const [nutritionData, setNutritionData] = useState(false)
+    const [stepTarget, setStepTarget] = useState(stepsTarget)
+    const [calorieTarget, setCalorieTarget] = useState(caloriesTarget)
+
+    const date = new Date()
+
+    const findMonth = (month) => {
+        switch (month) {
+            case 1:
+                return "Jan"
+                break;
+
+            case 2:
+                return "Feb"
+                break;
+
+            case 3:
+                return "Mar"
+                break;
+
+            case 4:
+                return "Apr"
+                break;
+
+            case 5:
+                return "May"
+                break;
+
+            case 6:
+                return "June"
+                break;
+
+            case 7:
+                return "July"
+                break;
+
+            case 8:
+                return "Aug"
+                break;
+
+            case 9:
+                return "Sept"
+                break;
+
+            case 10:
+                return "Oct"
+                break;
+
+            case 11:
+                return "Nav"
+                break;
+
+            case 12:
+                return "Dec"
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    const currentDate = date.getDate() + " " + findMonth(date.getMonth() + 1)
+
+    return (
+        <div key={id} className='bg-[#1E262F] mb-4 w-full h-[80px] rounded-[10px] relative ' >
+
+            {/* User Profile  */}
+            <div className='absolute left-[11px] top-[18px] flex items-center'>
+                <div className='mr-[18.32px] w-[40px] h-[40px]'><img className='rounded-full' src={userImage} alt='userimage' /></div>
+                <div>
+                    <p className=' text-white font-[500] text-[14px] leading-[17px] mb-[1px] '>{username}</p>
+                    <p className='text-[#BDBCBE] font-[500] text-[10px] leading-[12px]  '>{email}</p>
+                </div>
+            </div>
+
+            {/* Steps  */}
+            <div className='relative top-[5px] left-[750px] flex'>
+                <div className='w-[60px] h-[60px] absolute top-[8px]'>
+                    <CircularProgressbarWithChildren maxValue={stepTarget} value={stepsWalked} styles={buildStyles({
+                        strokeLinecap: "butt",
+                        textColor: "#BDBCBE",
+                        pathColor: "#7FD18C",
+                    })}>
+                        <p className='font-Mont font-[700] text-[12px] leading-[15px] text-white '>{stepsWalked}</p>
+                        <p className='text-[#BDBCBE] font-Mont font-[500] text-[8px] leading-[10px]'>Walked</p>
+                    </CircularProgressbarWithChildren>
+                </div>
+                <div className='absolute left-[74px] '>
+                    <button onClick={() => { setStepTarget(stepTarget + 500) }} className='bg-[#101317] px-[8px] py-[2px] rounded-[4px] text-white font-[700] absolute left-[2px]'><FiPlus className='h-[10px] w-[10px]' /></button>
+                    <p className='font-[700] text-[16px] leading-[20px] font-Mont text-white  absolute top-[21px] left-[4px]'>{(stepTarget / 1000)}k</p>
+                    <p className='font-[500] text-[10px] leading-[12px] text-[#BDBCBE] font-Mont absolute top-[37px] ' >target</p>
+                    <button onClick={() => { setStepTarget(stepTarget - 500) }} className='bg-[#101317] px-[8px] py-[2px] rounded-[4px] text-white font-[700] absolute top-[57px] left-[2px]'><FiMinus className='h-[10px] w-[10px]' /></button>
+                </div>
+            </div>
+
+            {/* Workout  */}
+            <div className='relative left-[930px] flex'>
+                <div className='absolute top-[14px] space-y-[8px]'>
+                    <div className='flex'>
+                        <div className='ml-[3px]'><Image src={date_icon} alt='' /></div>
+                        <p className='absolute left-[33px] font-Mont font-[600] text-[14px] leading-[17px] text-white w-[50px]'>{performedDate}</p>
+                    </div>
+                    <div className={`flex absolute top-[20px] left-[-9px] w-[99px] h-[30px] rounded-[8px] pt-[3.62px] pl-[9.25px] pr-[6px] ${currentDate === scheduledDate && 'bg-[#CC3838]'} `}>
+                        <div><TbCalendarTime size={20} className='text-white ' /></div>
+                        <p className='absolute left-[42px] font-Mont font-[600] text-[14px] leading-[17px] text-white w-[50px]'>{scheduledDate}</p>
+                    </div>
+                </div>
+                <div className='absolute left-[80px] '>
+                    {
+                        feedback ? <button className='bg-[#CC3838] px-[7px] py-[22.93px] rounded-[8px] text-white font-[700] absolute top-[10px] left-[18px]'><AiOutlineExclamation /></button>
+                            : <button className='bg-[#101317] px-[7px] py-[22.93px] rounded-[8px] text-white font-[700] absolute top-[10px] left-[18px]'><IoIosArrowForward /></button>
+                    }
+
+                </div>
+            </div>
+
+            {/* Nutrition  */}
+            <div className='relative top-[5px] left-[1110px] flex'>
+                <div onMouseOver={() => setNutritionData(true)} onMouseLeave={() => setNutritionData(false)} className='w-[70px] h-[70px] absolute top-[2px] cursor-pointer '>
+                    <PieChart
+                        style={{
+                            fontFamily:
+                                '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+                            fontSize: '8px',
+                        }}
+                        data={dataMock}
+                        radius={PieChart.defaultProps.radius - 6}
+                        lineWidth={30}
+                        segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+                        animate
+                        labelStyle={{
+                            fill: '#fff',
+                            opacity: 0.75,
+                            pointerEvents: 'none',
+                        }}
+                    />
+                    <p className='cursor-pointer font-Mont absolute top-[24px] left-[21px] font-[700] text-[12px] leading-[15px] text-white '>{calories}</p>
+                    <p className='cursor-pointer text-[#BDBCBE] absolute top-[36px] left-[19px] font-Mont font-[500] text-[8px] leading-[10px]'>calories</p>
+                </div>
+                {nutritionData &&
+                    <div className='w-[208px] h-[165px] rounded-[10px] z-50 absolute top-[70px] left-[-70px] text-white bg-[#333B44] '>
+                        {/* protein  */}
+                        <div className='w-[196.1px] h-[38.9px] bg-[#1B222A] absolute top-[13px] left-[5px] rounded-[4.86px] ' >
+                            <p className='absolute top-[4.86px] left-[8.91px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>PROTEIN</p>
+                            <p className='absolute top-[4.86px] right-[8.17px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>70g</p>
+                            <Progress mt={23.5} ml={8.91} mr={8.1} value={protein} color="#F45C84" label={`${protein}g`} size="md" radius="xl" />
+                        </div>
+                        {/* fats  */}
+                        <div className='w-[196.1px] h-[38.9px] bg-[#1B222A] absolute top-[64.05px] left-[5px] rounded-[4.86px] ' >
+                            <p className='absolute top-[4.86px] left-[8.91px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>FATS</p>
+                            <p className='absolute top-[4.86px] right-[8.17px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>70g</p>
+                            <Progress mt={23.5} ml={8.91} mr={8.1} value={fats} color="#03C6FA" label={`${fats}g`} size="md" radius="xl" />
+                        </div>
+                        {/* carbs  */}
+                        <div className='w-[196.1px] h-[38.9px] bg-[#1B222A] absolute top-[111.1px] left-[5px] rounded-[4.86px] ' >
+                            <p className='absolute top-[4.86px] left-[8.91px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>CARBS</p>
+                            <p className='absolute top-[4.86px] right-[8.17px] font-[500] text-white text-[10px] leading-[13px] font-Mont '>70g</p>
+                            <Progress mt={23.5} ml={8.91} mr={8.1} value={carbs} color="#F0C50F" label={`${carbs}g`} size="md" radius="xl" />
+                        </div>
+                    </div>
+                }
+                <div className='absolute left-[74px] '>
+                    <button onClick={() => setCalorieTarget(calorieTarget + 100)} className='bg-[#101317] px-[8px] py-[2px] rounded-[4px] text-white font-[700] absolute left-[2px]'><FiPlus className='h-[10px] w-[10px]' /></button>
+                    <p className='font-[700] text-[16px] leading-[20px] font-Mont text-white  absolute top-[21px] left-[4px]'>{calorieTarget / 1000}k</p>
+                    <p className='font-[500] text-[10px] leading-[12px] text-[#BDBCBE] font-Mont absolute top-[37px] ' >target</p>
+                    <button onClick={() => setCalorieTarget(calorieTarget - 100)} className='bg-[#101317] px-[8px] py-[2px] rounded-[4px] text-white font-[700] absolute top-[57px] left-[2px]'><FiMinus className='h-[10px] w-[10px]' /></button>
+                </div>
+                <div className='absolute left-[74px] '>
+                    <button className='bg-[#101317] px-[7px] py-[22.93px] rounded-[8px] text-white font-[700] absolute top-[4px] left-[50px]'><IoIosArrowForward /></button>
+                </div>
+            </div>
+
+            {/* Notifications */}
+            <div className='absolute left-[1350px] top-[20px] '>
+                <div className=' bg-[#36F5C7] pt-[10px] pb-[5px] pl-[10.12px] pr-[11.12px] rounded-[8px]  '><Image width={20} height={20} src={bell} alt='' /></div>
+            </div>
+        </div>
+    )
+}
+
+export default Row
---
+++ data/data.js
@@ -0,0 +1,45 @@
+export const allData = [
+    {
+        id: 1,
+        userImage: "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        username: "Charvie Sharma",
+        email: "charviesharma.cs@gmail.com",
+        stepsWalked: 3579,
+        stepsTarget: 5000,
+        performedDate: "15 July",
+        scheduledDate: "28 July",
+        calories: 2090,
+        caloriesTarget: 2500,
+        protein: 45,
+        fats: 30,
+        carbs: 50,
+        feedback: false,
+        dataMock : [
+            { title: 'Protien', value: 45, color: '#F45C84' },
+            { title: 'Fats', value: 30, color: '#03C6FA' },
+            { title: 'carbs', value: 50, color: '#F0C50F' },
+        ]
+    },
+    {
+        id: 2,
+        userImage: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        username: "Manoj Sharma",
+        email: "manojsharma.cs@gmail.com",
+        stepsWalked: 5282,
+        stepsTarget: 8000,
+        performedDate: "20 July",
+        scheduledDate: "29 July",
+        calories: 2708,
+        caloriesTarget: 4000,
+        protein: 65,
+        fats: 60,
+        carbs: 40,
+        feedback: true,
+        dataMock: [
+            { title: 'Protien', value: 65, color: '#F45C84' },
+            { title: 'Fats', value: 60, color: '#03C6FA' },
+            { title: 'carbs', value: 40, color: '#F0C50F' },
+        ]
+    },
+
+]
